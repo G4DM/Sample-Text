74ff6a8fac40179270d26819a67e2ce3082a0364
Gabriel Daniel Manea <gabimanea222@gmail.com>
2025-04-19 11:55:27 +0200

    Added public config.php file

---
+++ src/php/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$contrasena = ""; // <-- Pon aquí tu contraseña de MySQL para poder enlazar el formulario con la BBDD creada con el script .sql
+$basedatos = "sample_text";
+?>
